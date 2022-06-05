166630e9c0b84e608e6410e1a39d91c2b31db027
yabsera95 <yabserateferi@gmail.com>
2022-06-04 17:14:50 -0700

    to go

--- client/src/App.js
+++ client/src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
+        <p>Your Truffle Box is installed and ready  to go .</p>
         <h2>Smart Contract Example</h2>
       </div>
     );
